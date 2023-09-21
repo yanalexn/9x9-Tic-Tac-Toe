bbdfeeb4bd1955f83df8010ce0fc0a6db2ba33b2
Yan Alexander <yan.alexander.n@gmail.com>
2023-09-21 15:53:59 +0600

    init commit

---
+++ app/src/main/java/com/example/a9x9_tic_tac_toe/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.a9x9_tic_tac_toe
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "9x9-Tic-Tac-Toe"
+include(":app")
+ 
